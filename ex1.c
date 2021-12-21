ac2a53249dbf6610ee1c769ba3f03768f1993695
Joe Amburgey <jamburgey3@gatech.edu>
2021-12-20 19:22:49 -0500

    Commits from exercises 1 and 2

---
+++ ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	puts("Hello world.");
+	puts("Hello again\n");
+	puts("Hopefully this is on a new line\n\n");
+	puts("There should be a larger gap here.");
+	return 0;
+}
